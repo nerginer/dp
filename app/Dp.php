44e73807c8ec521eaa42662f9dd60feb73ad3e84
vagrant <vagrant@homestead>
2016-04-16 15:05:02 +0000

    first commit

--- app/Dp.php
+++ app/Dp.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Dp extends Model
 {
-    //
+      public function tags()
+    {
+       return $this->hasMany(tag::class);
+       
+    }
 }
